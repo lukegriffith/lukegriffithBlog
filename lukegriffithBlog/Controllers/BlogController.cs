58200bf87fa38bc40fcd275a1d4a586a0ee29377
Luke M Griffith <lukemgriffith@gmail.com>
2015-06-28 19:04:57 +0100

    razor work

---
+++ lukegriffithBlog/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lukegriffithBlog.Controllers;
+using lukegriffithBlog.Models;
+using lukegriffithBlog.ViewModels;
+
+namespace lukegriffithBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        private lukegriffithBlogContext db = new lukegriffithBlogContext();
+
+        
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var posts = db.Posts;
+            return View(posts);
+        }
+
+
+    }
+}
